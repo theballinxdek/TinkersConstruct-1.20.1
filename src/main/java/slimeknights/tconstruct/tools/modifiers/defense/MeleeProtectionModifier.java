acb3f2f56f3fcd7a4196f4b0e0e30cf10c644755
KnightMiner <knightminer4@gmail.com>
2024-04-29 15:15:27 -0400

    Swap new secondaries of melee prot and projectile prot
    Material thematics means they fit better with KR on iron and use item speed on cobalt

--- src/main/java/slimeknights/tconstruct/tools/modifiers/defense/MeleeProtectionModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/defense/MeleeProtectionModifier.java
@@ -1,22 +1,16 @@
 package slimeknights.tconstruct.tools.modifiers.defense;
 
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.ai.attributes.AttributeInstance;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier.Operation;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import slimeknights.mantle.data.predicate.damage.DamageSourcePredicate;
 import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.library.modifiers.data.ModifierMaxLevel;
 import slimeknights.tconstruct.library.modifiers.modules.armor.ProtectionModule;
 import slimeknights.tconstruct.library.module.ModuleHookMap.Builder;
 import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability.ComputableDataKey;
+import slimeknights.tconstruct.library.tools.capability.TinkerDataKeys;
 import slimeknights.tconstruct.library.tools.context.EquipmentChangeContext;
 
-import java.util.UUID;
-
 public class MeleeProtectionModifier extends AbstractProtectionModifier<ModifierMaxLevel> {
-  private static final UUID ATTRIBUTE_UUID = UUID.fromString("6f030b1e-e9e1-11ec-8fea-0242ac120002");
   private static final ComputableDataKey<ModifierMaxLevel> KEY = TConstruct.createKey("melee_protection", ModifierMaxLevel::new);
 
   public MeleeProtectionModifier() {
@@ -34,15 +28,8 @@ public class MeleeProtectionModifier extends AbstractProtectionModifier<Modifier
     float oldMax = data.getMax();
     super.set(data, slot, scaledLevel, context);
     float newMax = data.getMax();
-    // 5% bonus attack speed for the largest level
     if (oldMax != newMax) {
-      AttributeInstance instance = context.getEntity().getAttribute(Attributes.KNOCKBACK_RESISTANCE);
-      if (instance != null) {
-        instance.removeModifier(ATTRIBUTE_UUID);
-        if (newMax != 0) {
-          instance.addTransientModifier(new AttributeModifier(ATTRIBUTE_UUID, "tconstruct.melee_protection", 0.05 * newMax, Operation.ADDITION));
-        }
-      }
+      context.getTinkerData().ifPresent(d -> d.add(TinkerDataKeys.USE_SPEED_BONUS, (newMax - oldMax) * 0.05f));
     }
   }
 }
--- src/main/java/slimeknights/tconstruct/tools/modifiers/defense/ProjectileProtectionModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/defense/ProjectileProtectionModifier.java
@@ -1,16 +1,22 @@
 package slimeknights.tconstruct.tools.modifiers.defense;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier.Operation;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import slimeknights.mantle.data.predicate.damage.DamageSourcePredicate;
 import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.library.modifiers.data.ModifierMaxLevel;
 import slimeknights.tconstruct.library.modifiers.modules.armor.ProtectionModule;
 import slimeknights.tconstruct.library.module.ModuleHookMap.Builder;
 import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability.ComputableDataKey;
-import slimeknights.tconstruct.library.tools.capability.TinkerDataKeys;
 import slimeknights.tconstruct.library.tools.context.EquipmentChangeContext;
 
+import java.util.UUID;
+
 public class ProjectileProtectionModifier extends AbstractProtectionModifier<ModifierMaxLevel> {
+  private static final UUID ATTRIBUTE_UUID = UUID.fromString("6f030b1e-e9e1-11ec-8fea-0242ac120002");
   /** Entity data key for the data associated with this modifier */
   private static final ComputableDataKey<ModifierMaxLevel> PROJECTILE_DATA = TConstruct.createKey("projectile_protection", ModifierMaxLevel::new);
   public ProjectileProtectionModifier() {
@@ -28,8 +34,15 @@ public class ProjectileProtectionModifier extends AbstractProtectionModifier<Mod
     float oldMax = data.getMax();
     super.set(data, slot, scaledLevel, context);
     float newMax = data.getMax();
+    // 5% bonus attack speed for the largest level
     if (oldMax != newMax) {
-      context.getTinkerData().ifPresent(d -> d.add(TinkerDataKeys.USE_SPEED_BONUS, (newMax - oldMax) * 0.05f));
+      AttributeInstance instance = context.getEntity().getAttribute(Attributes.KNOCKBACK_RESISTANCE);
+      if (instance != null) {
+        instance.removeModifier(ATTRIBUTE_UUID);
+        if (newMax != 0) {
+          instance.addTransientModifier(new AttributeModifier(ATTRIBUTE_UUID, "tconstruct.melee_protection", 0.05 * newMax, Operation.ADDITION));
+        }
+      }
     }
   }
 }
